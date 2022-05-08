8311b231ffd2d00d553dfb71866e25c92df3a4d9
Nilesh Gavali <gavali.nilesh80186@gmail.com>
2022-05-08 19:16:12 +0530

    Update myclient.cc
    cleanup - latest version use Message call

--- p4go/cpp_files/myclient.cc
+++ p4go/cpp_files/myclient.cc
@@ -34,28 +34,7 @@ void MyClientUser::OutputText( const char *data, int length )
 
 StrBuf MyClientUser::MyData() 
 {
-    //cout << "MyClientUser::MyData" << my_output.Value()<< endl;
     return my_output;
-}
-
-void MyClientUser::OutputInfo( char level, const char *data )
-{
-    switch( level )
-    {
-        default:
-        case '0':
-            my_output.Append( data );
-            my_output.Append( "\n" );
-            break;
-        case '1':
-            my_output.Append( data );
-            my_output.Append( "\n" );
-            break;
-        case '2':
-            my_output.Append( data );
-            my_output.Append( "\n" );
-            break;
-    }
 }
 
 void MyClientUser::Message( Error *err )
@@ -63,5 +42,4 @@ void MyClientUser::Message( Error *err )
     StrBuf buf;
     err->Fmt( buf, EF_NEWLINE );
     my_output.Append( buf.Text() );
-    //cout << "MyClientUser::Message\n" << my_output.Text() << endl;
 }
