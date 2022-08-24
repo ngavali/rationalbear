b7c4f55e366f63b064e8ab5ad38c8116a82a2876
ngavali <ngavali@akamai.com>
2022-08-24 23:50:08 +0530

    using merge two link list

--- leet_code/go/merge-k-sorted-lists/merge-k-sorted-lists.go
+++ leet_code/go/merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -7,61 +7,91 @@ type ListNode struct {
 	Next *ListNode
 }
 
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	sortedList := &ListNode{}
+	sortedListHead := sortedList
+
+	for {
+
+		if list1 == nil {
+			sortedList.Next = list2
+			return sortedListHead.Next
+		}
+
+		if list2 == nil {
+			sortedList.Next = list1
+			return sortedListHead.Next
+		}
+
+		if list1.Val < list2.Val {
+			sortedList.Next = list1
+			list1 = list1.Next
+		} else {
+			sortedList.Next = list2
+			list2 = list2.Next
+		}
+		sortedList = sortedList.Next
+
+	}
+
+}
+
 func mergeKLists(lists []*ListNode) *ListNode {
 
 	if len(lists) == 0 {
 		return nil
 	}
 
-	listIndex := 0
-
-	var sortedListHead, sortedList *ListNode
-	is_lists_empty := false
-
-	for !is_lists_empty {
-
-		lowest := 10000
-		for i := 0; i < len(lists); i++ {
-			tmp := lists[i]
-
-			if tmp != nil {
-				if tmp.Val < lowest {
-					listIndex = i
-					lowest = tmp.Val
-				}
-			}
-		}
-
-		if lowest != 10000 {
-			if sortedList == nil {
-				sortedList = lists[listIndex]
-				sortedListHead = sortedList
-			} else {
-				sortedList.Next = lists[listIndex]
-				sortedList = sortedList.Next
-			}
-
-			lists[listIndex] = lists[listIndex].Next
-		}
-
-		is_lists_empty = true
-		for i := range lists {
-			if lists[i] != nil {
-				is_lists_empty = false
-				break
-			}
-		}
+	sortedList := lists[0]
+	for i := 1; i < len(lists); i++ {
+		sortedList = mergeTwoLists(sortedList, lists[i])
 	}
 
-	return sortedListHead
+	return sortedList
+
+}
+
+func XmergeKLists(lists []*ListNode) *ListNode {
+
+	if len(lists) == 0 {
+		return nil
+	}
+
+	sortedList := &ListNode{0, nil}
+	sortedListHead := sortedList
+
+	for {
+		listIndex := 0
+		lowest := 10001
+		updated := false
+
+		for i, l := range lists {
+			if l != nil && l.Val < lowest {
+				listIndex = i
+				lowest = l.Val
+				updated = true
+			}
+		}
+
+		if !updated {
+			break
+		}
+
+		sortedList.Next = lists[listIndex]
+		sortedList = sortedList.Next
+		lists[listIndex] = lists[listIndex].Next
+	}
+
+	return sortedListHead.Next
 
 }
 
 func main() {
 
-	//L1 := &ListNode{3, &ListNode{4, &ListNode{5, nil}}}
-	//L2 := &ListNode{2, &ListNode{3, &ListNode{4, nil}}}
-	//L3 := &ListNode{1, &ListNode{6, nil}}
-	//fmt.Println(mergeKLists([]*ListNode{L1, L2, L3}))
-	fmt.Println(mergeKLists([]*ListNode{{}, {}, {}}))
+	L1 := &ListNode{3, &ListNode{4, &ListNode{5, nil}}}
+	L2 := &ListNode{2, &ListNode{3, &ListNode{4, nil}}}
+	L3 := &ListNode{1, &ListNode{6, nil}}
+	fmt.Println(mergeKLists([]*ListNode{L1, L2, L3}))
+	fmt.Println(mergeKLists([]*ListNode{nil, nil, nil}))
+
 }
