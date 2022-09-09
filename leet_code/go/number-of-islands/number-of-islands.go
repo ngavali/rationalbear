f6019810f5ffd5dbafdc16f5fe43c04bff7be450
Nilesh Gavali <gavali.nilesh80186@gmail.com>
2022-09-09 22:37:27 +0530

    rust nix update

---
+++ leet_code/go/number-of-islands/number-of-islands.go
@@ -0,0 +1,15 @@
+package main
+
+func numIslands(grid [][]byte) int {
+
+    numIsland := 0
+    M := len(grid)
+    N := len(grid[0])
+    for i:=0; i<M; i++ {
+        for j:= 0; j<N ;j++ {
+            
+        }
+    }
+
+    return 0
+}
