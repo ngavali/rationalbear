5a5502b3f83e0aaeeb73b3fcec54a6cf544d8785
Nilesh Gavali <gavali.nilesh80186@gmail.com>
2025-09-15 10:47:25 +0530

    Single loop and use chars

--- leet_code/rust/count-and-say/src/main.rs
+++ leet_code/rust/count-and-say/src/main.rs
@@ -2,27 +2,29 @@
 struct Solution;
 
 impl Solution {
-    fn count_and_say_recursive(n: i32) -> String {
-        if n == 1 {
-            return "1".to_string();
-        }
-        let s = Self::count_and_say_recursive(n-1);
-        let mut s = s.chars();
-        let mut ans = String::from("");
-        let mut curr = s.next();
-        while curr != None {
-            let (mut j, mut n) = (0, curr);
-            while n == curr {
-                n = s.next();
-                j+=1;
+    pub fn count_and_say(n: i32) -> String {
+        let mut ans = String::from("1");
+        if n != 1 {
+            for _ in 1..n {
+                let mut c_ans = String::with_capacity(ans.len() * 2);
+                let mut s = ans.chars();
+                let mut curr = s.next();
+                let (mut j, mut n) = (0, curr);
+                while curr != None {
+                    if n == curr {
+                        n = s.next();
+                        j += 1;
+                    } else {
+                        //Handle last value too due to None check
+                        c_ans.push_str(format!("{j}{}", curr.unwrap()).as_str());
+                        curr = n;
+                        j = 0;
+                    }
+                }
+                ans = c_ans;
             }
-            ans.push_str(format!("{j}{}",curr.unwrap()).as_str());
-            curr = n;
         }
         ans
-    }
-    pub fn count_and_say(n: i32) -> String {
-        Self::count_and_say_recursive(n)
     }
 }
 
@@ -35,7 +37,16 @@ mod tests {
     use super::Solution;
 
     fn testcases() -> Vec<(i32, String)> {
-        vec![(4, "1211".to_string()), (1, "1".to_string())]
+        vec![
+            (4, "1211".to_string()),
+            (1, "1".to_string()),
+            (12, "3113112221232112111312211312113211".to_string()),
+            (
+                15,
+                "311311222113111231131112132112311321322112111312211312111322212311322113212221"
+                    .to_string(),
+            ),
+        ]
     }
     #[test]
     fn test_count_and_say() {
