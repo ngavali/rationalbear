f1410d5999316797d72e4b934f86a42335aa7c5e
Nilesh Gavali <gavali.nilesh80186@gmail.com>
2025-09-21 19:05:47 +0530

    Set and Hash + No need to remove from hash

--- leet_code/rust/design-task-manager/src/main.rs
+++ leet_code/rust/design-task-manager/src/main.rs
@@ -1,30 +1,21 @@
 //https://leetcode.com/problems/design-task-manager/
 
 use std::cell::RefCell;
+use std::collections::{HashMap,BTreeSet};
 
 #[derive(Debug)]
 struct TaskManager {
-    tasks: RefCell<Vec<Option<(i32, i32)>>>,
-    priority: RefCell<Vec<(i32, i32)>>,
+    tasks: RefCell<HashMap<i32,(i32, i32)>>,
+    priority: RefCell<BTreeSet<(i32, i32)>>,
 }
 
-/** 
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl TaskManager {
     fn new(tasks: Vec<Vec<i32>>) -> Self {
-        let mut t = vec![None;100001];
-        let mut p = Vec::with_capacity(tasks.len());
+        let mut t = HashMap::new();
+        let mut p : BTreeSet<(i32,i32)> = BTreeSet::new();
         tasks.iter().for_each(|a| {
-            t[a[1] as usize] = Some((a[2], a[0]));
-            p.push((a[2], a[1]));
-        });
-        p.sort_by(|a, b| {
-            if a.0 == b.0 {
-                return a.1.cmp(&b.1);
-            }
-            a.0.cmp(&b.0)
+            t.insert(a[1],(a[2], a[0]));
+            p.insert((a[2], a[1]));
         });
         TaskManager {
             tasks: RefCell::new(t),
@@ -35,79 +26,46 @@ impl TaskManager {
     fn add(&self, user_id: i32, task_id: i32, priority: i32) {
         let mut t = self.tasks.borrow_mut();
         let mut p = self.priority.borrow_mut();
-        let pos = match p.binary_search_by(|a| {
-            if a.0 == priority {
-                a.1.cmp(&task_id)
-            } else {
-                a.0.cmp(&priority)
-            }
-        }) {
-            Ok(idx) => idx,
-            Err(idx) => idx,
-        };
-        p.insert(pos, (priority, task_id));
-        t[task_id as usize] = Some((priority, user_id));
+        p.insert((priority, task_id));
+        t.insert(task_id,(priority, user_id));
     }
 
     fn edit(&self, task_id: i32, new_priority: i32) {
         let mut t = self.tasks.borrow_mut();
-        if let Some((priority, user_id)) = t[task_id as usize] {
+        if let Some((priority, user_id)) = t.get_mut(&task_id) {
             let mut tp = self.priority.borrow_mut();
-            match tp.binary_search_by(|a| {
-                if a.0 == priority {
-                        a.1.cmp(&task_id)
-                } else {
-                    a.0.cmp(&priority)
-                }
-            }) {
-                Ok(pos) => {
-                    let mut e = tp.remove(pos);
-                    let pos = match tp.binary_search_by(|a| {
-                        if a.0 == new_priority {
-                            a.1.cmp(&task_id)
-                        } else {
-                            a.0.cmp(&new_priority)
-                        }
-                    }) {
-                        Ok(idx) => idx,
-                        Err(idx) => idx,
-                    };
-                    tp.insert(pos,(new_priority, task_id));
-                    t[task_id as usize] = Some((new_priority, task_id));
-                },
-                Err(idx) => {},
-            };
+            tp.remove(&(*priority, task_id));
+            tp.insert((new_priority, task_id));
+            *priority = new_priority;
         }
     }
 
     fn rmv(&self, task_id: i32) {
         let mut t= self.tasks.borrow_mut();
         let mut p= self.priority.borrow_mut();
-        t[task_id as usize] = None;
-        p.retain(|p| p.1 != task_id);
+        if let Some((priority, user_id)) = t.get(&task_id) {
+            p.remove(&(*priority, task_id));
+        }
     }
 
     fn exec_top(&self) -> i32 {
         let mut t = self.tasks.borrow_mut();
         let mut p  = self.priority.borrow_mut();
-        if let Some(task) = p.pop() {
-            if let Some((priority,user_id)) = t[task.1 as usize] {
-                t[task.1 as usize] = None;
-                return user_id;
+        let mut res = None;
+        if let Some((_, tid)) = p.pop_last() {
+            if let Some((_, uid)) = t.get(&tid) {
+                res = Some((tid, *uid));
             }
         }
-        -1
+        match res {
+            Some((tid,uid)) => {
+                uid
+            },
+            _ => -1,
+        }
     }
 }
 
-/**
- * Your TaskManager object will be instantiated and called as such:
- * let obj = TaskManager::new(tasks);
- * obj.add(userId, taskId, priority);
- * obj.edit(taskId, newPriority);
- * obj.rmv(taskId);
- * let ret_4: i32 = obj.exec_top();
- */
 fn main() {
     println!("Hello, world!");
 }
