3384bd00230deee8aff26e6f4a8cc327cf717d4a
Nilesh Gavali <gavali.nilesh80186@gmail.com>
2025-09-28 19:31:28 +0530

    cleanup

--- leet_code/rust/palindrome-partitioning/src/main.rs
+++ leet_code/rust/palindrome-partitioning/src/main.rs
@@ -3,74 +3,50 @@
 struct Solution;
 
 impl Solution {
-    fn is_palindrome(s: &[u8], start: usize, end: usize, dp: &mut Vec<Vec<bool>>) -> bool {
-        if end - start >= 2  {
-            return dp[start+1][end-1] && s[start] == s[end];
+    fn is_palindrome(s: &[char], mut i: usize, mut j:usize) -> bool {
+        while i < j {
+            if s[i] != s[j] {
+                return false;
+            }
+            i+=1;
+            j-=1;
         }
-        dp[start][end]
+        return true;
     }
     fn generate_palindromic_partitions(
         palindromic_partitions: &mut Vec<Vec<String>>,
         start: usize,
-        s: &[u8],
+        s: &[char],
         curr_list: &mut Vec<String>,
-        dp: &mut Vec<Vec<bool>>
     ) {
         if start >= s.len() {
             palindromic_partitions.push(curr_list.clone());
             return;
         }
         for k in start..s.len() {
-            println!("{} {}", start, k);
-            if Self::is_palindrome(&s, start, k, dp) {
-                dp[start][k] = true;
-                curr_list.push(String::from_utf8(s[start..(k+1)].to_vec()).unwrap());
+            if Self::is_palindrome(s, start, k) {
+                curr_list.push(String::from_iter(&s[start..(k+1)]));
                 Self::generate_palindromic_partitions(
                     palindromic_partitions,
                     k+1,
                     s,
                     curr_list,
-                    dp
                 );
                 curr_list.pop();
             }
-            /*
-            if start + k <= s.len() && Self::is_palindrome(&s, start, start + k - 1, dp) {
-                dp[start][start + k -1] = true;
-                curr_list.push(String::from_utf8(s[start..start + k].to_vec()).unwrap());
-                Self::generate_palindromic_partitions(
-                    palindromic_partitions,
-                    start + k,
-                    s,
-                    curr_list,
-                    dp
-                );
-                curr_list.pop();
-            }*/
         }
     }
 
     pub fn partition(s: String) -> Vec<Vec<String>> {
         //Generate palindromic_partitions
-        let s = s.as_bytes();
+        let s = s.chars().collect::<Vec<char>>();
         let mut palindromic_partitions: Vec<Vec<String>> = Vec::with_capacity(s.len());
         let mut curr_list: Vec<String> = Vec::new();
-        let mut dp = vec![vec![false;s.len()];s.len()];
-
-        for i in 0..s.len() {
-            dp[i][i] = true;
-        }
-        for i in 0..s.len() - 1 {
-            if s[i] == s[i + 1] {
-                dp[i][i + 1] = true;
-            }
-        }
         Self::generate_palindromic_partitions(
             &mut palindromic_partitions,
             0,
-            s,
+            &s,
             &mut curr_list,
-            &mut dp
         );
         palindromic_partitions
     }
