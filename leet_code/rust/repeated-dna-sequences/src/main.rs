c9242cb5ac1d00964a82e13b3e7daf97d44f855a
Nilesh Gavali <gavali.nilesh80186@gmail.com>
2025-10-17 13:11:46 +0530

    Second pass

--- leet_code/rust/repeated-dna-sequences/src/main.rs
+++ leet_code/rust/repeated-dna-sequences/src/main.rs
@@ -5,20 +5,33 @@ struct Solution;
 use std::collections::HashMap;
 
 impl Solution {
+    fn encode(s: &[char]) -> usize {
+        let mut number = 0;
+        for c in s {
+            match c {
+                'C' => number |= 0b01,
+                'G' => number |= 0b10,
+                'T' => number |= 0b11,
+                _ => {}
+            };
+            number <<= 2;
+        }
+        number
+    }
     pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
         let mut ans = Vec::new();
+        let mut str_map = vec![-1; 4194301];
         if s.len() > 10 {
-            let mut seq_freq = HashMap::<String, bool>::with_capacity(s.len());
-            let s: Vec<char> = s.chars().collect();
+            let s = s.chars().collect::<Vec<char>>();
             for i in 0..s.len() - 9 {
-                let s = String::from_iter(&s[i..i + 10]);
-                if let Some(freq) = seq_freq.get_mut(&s) {
-                    if !*freq {
-                        ans.push(s);
-                        *freq = true;
-                    }
-                } else {
-                    seq_freq.insert(s, false);
+                let encoded_value = Self::encode(&s[i..i+10]);
+                if str_map[encoded_value] == -1 {
+                    str_map[encoded_value] = 0;
+                } else if str_map[encoded_value] == 0{
+                    str_map[encoded_value] = 1;
+                    let ss = String::from_iter(&s[i..i + 10]);
+                println!("String: {ss} -> Encoded value: {encoded_value:10}" );
+                    ans.push(ss);
                 }
             }
         }
@@ -34,6 +47,7 @@ fn main() {
 mod tests {
     fn testcases() -> Vec<(String, Vec<String>)> {
         vec![
+            ("TTTTTTTTTTT".to_string(), vec!["TTTTTTTTTT".to_string()]),
             (
                 "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT".to_string(),
                 vec!["AAAAACCCCC".to_string(), "CCCCCAAAAA".to_string()],
@@ -48,7 +62,7 @@ mod tests {
     #[test]
     fn test_find_repeated_dna_sequences() {
         for (s, want) in testcases() {
-            assert_eq!( Solution::find_repeated_dna_sequences(s), want );
+            assert_eq!(Solution::find_repeated_dna_sequences(s), want);
         }
     }
 }
