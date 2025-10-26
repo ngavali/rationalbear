16643df22ec463058892669349041309fe259fa8
Nilesh Gavali <gavali.nilesh80186@gmail.com>
2025-10-26 17:57:26 +0530

    Finally 3ms best solution

--- leet_code/rust/cherry-pickup/src/main.rs
+++ leet_code/rust/cherry-pickup/src/main.rs
@@ -1,54 +1,52 @@
 //https://leetcode.com/problems/cherry-pickup/
 struct Solution;
 struct SolutionBt;
-struct SolutionDpToTab;
+struct SolutionDp;
 
-impl SolutionDpToTab {
+impl SolutionDp {
     pub fn cherry_pickup(grid: Vec<Vec<i32>>) -> i32 {
         let n = grid.len();
-        let m = grid[0].len();
-        let n_1 = n - 1;
-        let m_1 = m - 1;
-        let mut dp = vec![vec![vec![i32::MIN; n]; m]; n];
-
-        for r1 in (0..n).rev() {
-            for c1 in (0..m).rev() {
-                for r2 in (0..n).rev() {
-                    if r1 + c1 < r2 {
+        //Contraints 1 <= n <= 50, max is 2 * 50 - 2 steps, so twice is 198 pickups max
+        let mut prev = vec![vec![-200; grid.len()]; grid.len()];
+        prev[0][0] = grid[0][0];
+        for step in 1..=2 * n - 2 {
+            let l = if step >= n { step - n + 1 } else { 0 };
+            let mut next = vec![vec![i32::MIN; n]; n];
+            for r1 in l..n.min(step + 1) {
+                for r2 in l..n.min(step + 1) {
+                    let (c1, c2) = (step - r1, step - r2);
+                    if grid[r1][c1] == -1 || grid[r2][c2] == -1 {
                         continue;
                     }
-                    let c2 = r1 + c1 - r2;
-                    if c2 >= m || grid[r1][c1] == -1 || grid[r2][c2] == -1 {
-                        continue;
-                    }
-                    if r1 == n_1 && c1 == m_1 {
-                        dp[r1][c1][r2] = grid[r1][c1];
-                        continue;
-                    }
-                    let mut cc = i32::MIN;
-                    if r1 < n_1 {
-                        if r2 < n_1 {
-                            cc = cc.max(dp[r1 + 1][c1][r2 + 1]);
-                        }
-                        cc = cc.max(dp[r1 + 1][c1][r2]);
-                    }
-                    if c1 < m_1 {
-                        if r2 < m_1 {
-                            cc = cc.max(dp[r1][c1 + 1][r2 + 1]);
-                        }
-                        cc = cc.max(dp[r1][c1 + 1][r2]);
-                    }
-                    dp[r1][c1][r2] = cc + grid[r1][c1];
+                    let mut cherries_picked = grid[r1][c1];
                     if r1 != r2 {
-                        dp[r1][c1][r2] += grid[r2][c2];
+                        cherries_picked += grid[r2][c2];
                     }
+                    let prev_picked_cherries = prev[r1][r2].max(
+                    if r1 > 0 {
+                        prev[r1 - 1][r2].max(
+                            if r2 > 0 {
+                                prev[r1 - 1][r2 - 1]
+                            } else {
+                                -200
+                            }
+                        )
+                    } else {
+                        -200
+                    }).max(
+                    if r2 > 0 {
+                        prev[r1][r2 - 1]
+                    } else {
+                        -200
+                    });
+                    next[r1][r2] = cherries_picked + prev_picked_cherries;
                 }
             }
+            std::mem::swap(&mut prev, &mut next);
         }
-        if dp[0][0][0] > 0 {
-            dp[0][0][0]
-        } else {
-            0
+        match prev[n-1][n-1] {
+            res if res > 0 => res,
+            _ => 0,
         }
     }
 }
@@ -215,11 +213,12 @@ mod tests {
             assert_eq!(SolutionBt::cherry_pickup(grid), want);
         }
     }
-    use super::SolutionDpToTab;
+
+    use super::SolutionDp;
     #[test]
-    fn test_cherry_pickup_bt_to_tab() {
-        for (grid, want) in testcases().into_iter().take(6) {
-            assert_eq!(SolutionDpToTab::cherry_pickup(grid), want);
+    fn test_cherry_pickup_dp() {
+        for (grid, want) in testcases().into_iter() {
+            assert_eq!(SolutionDp::cherry_pickup(grid), want);
         }
     }
 
