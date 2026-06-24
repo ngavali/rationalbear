3f6e65648b0c57bec3055665406ebe1ff02c0b78
Vihaan Gavali <vgavali@Vihaans-MacBook-Air.local>
2026-06-24 21:19:05 +0530

    100% final

--- leet_code/rust/unique-paths-ii/src/main.rs
+++ leet_code/rust/unique-paths-ii/src/main.rs
@@ -1,39 +1,99 @@
 struct Solution {}
 
+use std::mem;
 impl Solution {
-    fn dfs(obstacle_grid: &Vec<Vec<i32>>, x: usize, y: usize, m: usize, n: usize, memo: &mut Vec<Vec<i32>>) -> i32 {
+    fn dfs(
+        obstacle_grid: &Vec<Vec<i32>>,
+        x: usize,
+        y: usize,
+        m: usize,
+        n: usize,
+        memo: &mut Vec<Vec<i32>>,
+    ) -> i32 {
         //println!("At position: x={} y={} (m={} n={})", x, y, m, n);
         let mut res = 0;
-        if (obstacle_grid[x][y] == 1) {
+        if (x == m || y == n || obstacle_grid[x][y] == 1) {
             return res;
         }
-        if (x == m - 1 && y == n - 1) {
-            //println!("Reached !!!");
-            return 1;
-        }
-        if memo[x][y] != -1 {
-            return memo[x][y];
-        }
-        if (x < m - 1) {
+
+        if (!(x == m - 1 && y == n - 1)) {
+            if memo[x][y] != -1 {
+                return memo[x][y];
+            }
             res += Self::dfs(obstacle_grid, x + 1, y, m, n, memo);
-        }
-        if (y < n - 1) {
             res += Self::dfs(obstacle_grid, x, y + 1, m, n, memo);
+            memo[x][y] = res;
+            return res;
         }
-        memo[x][y] = res;
-        res
+
+        //println!("Reached !!!");
+        return 1;
     }
+    fn dfs_to_dp(
+        obstacle_grid: &Vec<Vec<i32>>,
+        x: usize,
+        y: usize,
+        m: usize,
+        n: usize,
+        memo: &mut Vec<Vec<i32>>,
+    ) -> i32 {
+        let mut dp = vec![vec![0; n]; m];
+        let mut prev = vec![0; n];
+        for x in (0..m).rev() {
+            let mut curr = vec![0; n];
+            for y in (0..n).rev() {
+                if (obstacle_grid[x][y] == 1) {
+                    dp[x][y] = 0;
+                    curr[y] = 0;
+                    continue;
+                }
+
+                if (x == m - 1 && y == n - 1) {
+                    dp[x][y] = 1;
+                    curr[y] = 1;
+                    continue;
+                }
+                if (x < m - 1) {
+                    dp[x][y] += dp[x + 1][y];
+                }
+                if (y < n - 1) {
+                    dp[x][y] += dp[x][y + 1];
+                curr[y] += curr[y + 1];
+                }
+                //No x bound check required here!
+                curr[y] += prev[y];
+            }
+            mem::swap(&mut curr,&mut prev);
+        }
+        //println!("{:?}", dp);
+        //println!("{:?}", prev);
+        //dp[0][0]
+        prev[0]
+    }
+
     pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
-        let (m,n) = (obstacle_grid.len(), obstacle_grid[0].len());
-        let mut memo = vec![vec![-1; n]; m];
-        println!("{:?}", memo);
-        Self::dfs(
-            &obstacle_grid,
-            0,
-            0,
-            m, n,
-            &mut memo,
-        )
+        let (m, n) = (obstacle_grid.len(), obstacle_grid[0].len());
+        let mut obstacle_grid = obstacle_grid;
+        for x in (0..m).rev() {
+            let mut curr = vec![0; n];
+            for y in (0..n).rev() {
+                if (obstacle_grid[x][y] == 1) {
+                    obstacle_grid[x][y] = 0;
+                    continue;
+                }
+                if (x == m - 1 && y == n - 1) {
+                    obstacle_grid[x][y] = 1;
+                    continue;
+                }
+                if (x < m - 1) {
+                    obstacle_grid[x][y] += obstacle_grid[x + 1][y];
+                }
+                if (y < n - 1) {
+                    obstacle_grid[x][y] += obstacle_grid[x][y + 1];
+                }
+            }
+        }
+        obstacle_grid[0][0]
     }
 }
 
